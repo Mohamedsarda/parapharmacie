414d7c48b6be44b6da516485e3744434f5f263d4
unknown <cbamiixsimo@gmail.com>
2022-09-06 11:05:29 +0100

    login page

--- app/src/admin/pages/Login.jsx
+++ app/src/admin/pages/Login.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./scss/login.scss";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/adminAuthentication/v1/signIn", {
+        adminEmail: email,
+        adminPassword: password,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return (
     <div className="login">
-      <form>
+      <form onSubmit={handleLogin}>
         <div className="row">
           <label htmlFor="">Email</label>
-          <input type="email" />
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
         </div>
         <div className="row">
           <label htmlFor="">Password</label>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
         </div>
-        <button>Login</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
