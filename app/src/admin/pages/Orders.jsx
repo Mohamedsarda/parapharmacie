8bd1aae4f11fb5a8ee01c17d8282c28129ab8de4
unknown <cbamiixsimo@gmail.com>
2022-09-09 20:21:00 +0100

    orders table / sidebar fixed :)

--- app/src/admin/pages/Orders.jsx
+++ app/src/admin/pages/Orders.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import NavBar from "../components/Navbar";
 import SideBar from "../components/Sidebar";
 import Loading from "../components/loading";
+import Table from "../components/Table";
 
 const Marques = ({ signOut }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <div className="categories">
       {isLoading ? (
@@ -12,6 +13,7 @@ const Marques = ({ signOut }) => {
           <SideBar signOut={signOut} />
           <div className="categotiesContainer">
             <NavBar />
+            <Table />
           </div>
         </>
       ) : (
